22e29ff03e81dac959d0fbfbb9e45c5ed80c8953
Halyna2023 <halyna.oli2023@gmail.com>
2023-10-30 22:04:49 +0200

    Add HW1
    Signed-off-by: Halyna2023 <halyna.oli2023@gmail.com>

--- src/ua/olifirenko/hw1/HW1_task3.java
+++ src/ua/olifirenko/hw1/HW1_task3.java
@@ -12,26 +12,8 @@ public class HW1_task3 {
 
     }
 
-    //Вывести нечетное число
-    //Задача
-    //Из двух чисел с разной четностью вывести на экран нечетное число.
-    public static class Task2 {
 
-        public static void main(String[] args) {
-            int x1 = 10;
-            int x2 = 17;
-            printOdd(x1, x2);
-        }
 
-        public static void printOdd(int y1, int y2) {
-            if (y1 % 2 > 0) {
-                System.out.println(y1);
-            } else {
-                System.out.println(y2);
-            }
-        }
-    }
+
 }
-
-
 
