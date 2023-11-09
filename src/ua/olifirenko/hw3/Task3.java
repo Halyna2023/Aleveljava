0c8d7efe769217b4983f7222afac28fb3d2a7a56
Halyna2023 <halyna.oli2023@gmail.com>
2023-11-09 19:37:32 +0200

    Add HW3 Task3
    Signed-off-by: Halyna2023 <halyna.oli2023@gmail.com>

--- src/ua/olifirenko/hw3/Task3.java
+++ src/ua/olifirenko/hw3/Task3.java
@@ -5,10 +5,21 @@ package ua.olifirenko.hw3;
 
 public class Task3 {
     public static void main(String[] args) {
-        String inputString = "Вводится строка, состоящая из слов. Требуется посчитать количество слов в ней.";
-        String[] words = inputString.split("\\s+");
-        int wordCount = words.length;
-        System.out.println("Number of words in the string: " + wordCount);
+        String inputString = " Вводится строка, состоящая из слов Требуется посчитать количество слов в ней.";
+        System.out.println(inputString);
+        String stringWithoutSpaces = inputString.replace(" ", "");
+        System.out.println(stringWithoutSpaces);
+
+        String trimmedString = inputString.trim();
+        System.out.println(trimmedString.split(" ").length);
+
+
+//        String inputString = "    Вводится    строка,   состоящая    из слов. Требуется посчитать количество слов в ней.";
+//        String trimmedString = inputString.trim();
+//        String[] words = inputString.split("\\s+");
+//
+//        int wordCount = words.length;
+//        System.out.println("Number of words in the string: " + wordCount);
     }
 }
 
