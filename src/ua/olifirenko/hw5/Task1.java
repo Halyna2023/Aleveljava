094dcc382e5afa36118c4f04db218b93145729b2
Halyna2023 <halyna.oli2023@gmail.com>
2023-11-27 13:11:42 +0200

    Add HW5 Task1.java Вариант 2 с учётом замечаний
    Signed-off-by: Halyna2023 <halyna.oli2023@gmail.com>

--- src/ua/olifirenko/hw5/Task1.java
+++ src/ua/olifirenko/hw5/Task1.java
@@ -9,38 +9,75 @@ package ua.olifirenko.hw5;
 //        *Передать массив в метод, заполнить данный массив
 public class Task1 {
 
-        public static void main(String[] args) {
-            // Размеры двумерного массива
-            int rows = 5;
-            int cols = 5;
+//        public static void main(String[] args) {
+//            // Размеры двумерного массива
+//            int rows = 5;
+//            int cols = 5;
+//
+//            // Создаем двумерный массив
+//            int[][] array = new int[rows][cols];
+//
+//            // Заполняем массив значениями индекса
+//            for (int i = 0; i < rows; i++) {
+//                for (int j = 0; j < cols; j++) {
+//                    array[i][j] = i * cols + j + 1;
+//                }
+//            }
+//
+//            // Каждая вторая строка заполняется отрицательными значениями
+//            for (int i = 1; i < rows; i += 2) {
+//                for (int j = 0; j < cols; j++) {
+//                    array[i][j] = -array[i][j];
+//                }
+//            }
+//
+//            // Выводим результаты
+//            System.out.println("Двумерный массив:");
+//            for (int i = 0; i < rows; i++) {
+//                for (int j = 0; j < cols; j++) {
+//                    System.out.print(array[i][j] + "\t");
+//                }
+//                System.out.println();
+//            }
+//        }
 
-            // Создаем двумерный массив
-            int[][] array = new int[rows][cols];
 
-            // Заполняем массив значениями индекса
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < cols; j++) {
-                    array[i][j] = i * cols + j + 1;
-                }
-            }
+    public static void main(String[] args) {
+        int rows = 4;
+        int cols = 3;
+        int[][] array = new int[rows][cols];
 
-            // Каждая вторая строка заполняется отрицательными значениями
-            for (int i = 1; i < rows; i += 2) {
-                for (int j = 0; j < cols; j++) {
-                    array[i][j] = -array[i][j];
-                }
-            }
+        fillArray(array);
 
-            // Выводим результаты
-            System.out.println("Двумерный массив:");
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < cols; j++) {
-                    System.out.print(array[i][j] + "\t");
-                }
-                System.out.println();
+        // Выводим заполненный массив
+        printArray(array);
+    }
+
+    // Метод для заполнения двумерного массива значениями индекса
+    // и отрицательными значениями в каждой второй строке
+    private static void fillArray(int[][] array) {
+        int counter = 1;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = (i % 2 == 0) ? counter : -counter;
+                counter++;
             }
         }
     }
+
+    // Метод для вывода двумерного массива
+    private static void printArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
 
 
 
