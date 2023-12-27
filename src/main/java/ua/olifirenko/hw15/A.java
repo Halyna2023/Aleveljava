b421214709d942c69b040070f7587bc4b00eeb2c
Halyna2023 <halyna.oli2023@gmail.com>
2023-12-27 20:22:13 +0200

    add HW15  Task1
    Signed-off-by: Halyna2023 <halyna.oli2023@gmail.com>

--- src/main/java/ua/olifirenko/hw15/A.java
+++ src/main/java/ua/olifirenko/hw15/A.java
@@ -1,7 +1,21 @@
 package ua.olifirenko.hw15;
 
 import java.lang.reflect.Field;
-
+//1. Создать экземпляр класса с приватным конструктором и изменить значение
+//        переменных:
+//public class A {
+//    private String id = "123";
+//    private String name = "A";
+//    private A() {
+//    }
+//    @Override
+//    public String toString() {
+//        return "A{" +
+//                "id='" + id + '\'' +
+//                ", name='" + name + '\'' +
+//                '}';
+//    }
+//}
 
 public final class A {
     private String id = "123";
@@ -25,8 +39,5 @@ public final class A {
                 '}';
     }
 
-    public static void main(String[] args) {
-        A modifiedInstance = A.createAndModify();
-        System.out.println(modifiedInstance);
-    }
+
 }
---
+++ src/main/java/ua/olifirenko/hw15/Task1.java
@@ -0,0 +1,8 @@
+package ua.olifirenko.hw15;
+
+public class Task1 {
+    public static void main(String[] args) {
+        A modifiedInstance = A.createAndModify();
+        System.out.println(modifiedInstance);
+    }
+}
