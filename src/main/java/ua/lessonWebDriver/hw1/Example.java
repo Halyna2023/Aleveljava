7b53a5e6e152da6eaba8767f1a6d0ee274ac38e4
Halyna2023 <halyna.oli2023@gmail.com>
2023-12-27 21:16:29 +0200

    add Automation hw1
    Signed-off-by: Halyna2023 <halyna.oli2023@gmail.com>

---
+++ src/main/java/ua/lessonWebDriver/hw1/Example.java
@@ -0,0 +1,19 @@
+package ua.lessonWebDriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Example {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://mvnrepository.com/");
+        WebElement input = driver.findElement(By.xpath("//input[@id='query]"));
+        WebElement searchButton = driver.findElement(By.xpath("//input[@value='Search']"));
+        input.clear();
+        searchButton.sendKeys("selenium");
+        searchButton.click();
+        driver.quit();
+    }
+}
