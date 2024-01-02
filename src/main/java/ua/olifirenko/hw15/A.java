e9c0f92cb5210e9350bb1007e52914d1b9b621c1
Halyna2023 <halyna.oli2023@gmail.com>
2024-01-02 19:25:33 +0200

    add HW15  Task1
    Signed-off-by: Halyna2023 <halyna.oli2023@gmail.com>

--- src/main/java/ua/olifirenko/hw15/A.java
+++ src/main/java/ua/olifirenko/hw15/A.java
@@ -1,36 +1,10 @@
 package ua.olifirenko.hw15;
 
-import java.lang.reflect.Field;
-//1. Создать экземпляр класса с приватным конструктором и изменить значение
-//        переменных:
-//public class A {
-//    private String id = "123";
-//    private String name = "A";
-//    private A() {
-//    }
-//    @Override
-//    public String toString() {
-//        return "A{" +
-//                "id='" + id + '\'' +
-//                ", name='" + name + '\'' +
-//                '}';
-//    }
-//}
-
-public final class A {
+public class A {
     private String id = "123";
     private String name = "A";
-
     private A() {
     }
-
-    public static A createAndModify() {
-        A instance = new A();
-        instance.id = "456";
-        instance.name = "Modified A";
-        return instance;
-    }
-
     @Override
     public String toString() {
         return "A{" +
@@ -38,6 +12,4 @@ public final class A {
                 ", name='" + name + '\'' +
                 '}';
     }
-
-
 }
--- src/main/java/ua/olifirenko/hw15/Task1.java
+++ src/main/java/ua/olifirenko/hw15/Task1.java
@@ -1,8 +1,44 @@
 package ua.olifirenko.hw15;
-
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 public class Task1 {
-    public static void main(String[] args) {
-        A modifiedInstance = A.createAndModify();
-        System.out.println(modifiedInstance);
+
+
+    public static class Main {
+        public static void main(String[] args) throws Exception {
+            // Создание экземпляра класса A с помощью рефлексии
+            A instance = createInstance(A.class);
+
+            // Изменение значения приватного поля id
+            setPrivateField(instance, "id", "456");
+
+            // Изменение значения приватного поля name
+            setPrivateField(instance, "name", "B");
+
+            // Вывод измененного экземпляра
+            System.out.println(instance);
+        }
+
+        // Метод для создания экземпляра класса с приватным конструктором
+        private static <T> T createInstance(Class<T> clazz) throws Exception {
+            java.lang.reflect.Constructor<T> constructor = clazz.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        }
+
+        // Метод для установки значения приватного поля
+        private static void setPrivateField(Object object, String fieldName, Object value) throws Exception {
+            Field field = object.getClass().getDeclaredField(fieldName);
+            field.setAccessible(true);
+
+            // Убедитесь, что поле не является final (не изменяемым)
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+
+            // Установка нового значения поля
+            field.set(object, value);
+        }
     }
+
 }
